f451e7826b4ffb4bfdc473b9917bfc12d8898520
akhihaani <akhihaani@gmail.com>
2026-07-27 15:59:09 +0100

    Fixed typo in cert_man helm_release

--- Terraform/infra/modules/eks-addons/eks-addons.tf
+++ Terraform/infra/modules/eks-addons/eks-addons.tf
@@ -28,7 +28,7 @@ resource "helm_release" "cert_manager" {
   version          = "v1.20.0"
   create_namespace = true
   namespace        = "cert-manager"
-  depends_on       = [module.cert_manager_rsa_role.iam_role_arn]
+  depends_on       = [module.cert_manager_irsa_role.iam_role_arn]
 
   # First entry: cert-manager waits for the IAM role to be created.
   # Second entry: installs the custom resource definitions.
