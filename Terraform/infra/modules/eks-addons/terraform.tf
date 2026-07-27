0d2b13f8f2d8076da708ca8515988b833cfd17a4
akhihaani <akhihaani@gmail.com>
2026-07-27 15:30:46 +0100

    Moved ClusterIssuer to separate manifests folder

--- Terraform/infra/modules/eks-addons/terraform.tf
+++ Terraform/infra/modules/eks-addons/terraform.tf
@@ -3,6 +3,5 @@ terraform {
   required_providers {
     aws        = { source = "hashicorp/aws", version = "~> 6.0" }
     helm       = { source = "hashicorp/helm", version = "~> 3.0" }
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.0" }
   }
 }
